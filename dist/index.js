c360bac263049aebcc75223f8aac187f72bb4082
steveoni <steohenoni2@gmail.com>
2021-01-18 20:26:55 +0100

    [index][s]: add main index for build files

---
+++ dist/index.js
@@ -0,0 +1,55 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Metadata", {
+  enumerable: true,
+  get: function get() {
+    return _Metadata.default;
+  }
+});
+Object.defineProperty(exports, "ProgressBar", {
+  enumerable: true,
+  get: function get() {
+    return _ProgressBar.default;
+  }
+});
+Object.defineProperty(exports, "TablePreview", {
+  enumerable: true,
+  get: function get() {
+    return _TablePreview.default;
+  }
+});
+Object.defineProperty(exports, "TableSchema", {
+  enumerable: true,
+  get: function get() {
+    return _TableSchema.default;
+  }
+});
+Object.defineProperty(exports, "Upload", {
+  enumerable: true,
+  get: function get() {
+    return _Upload.default;
+  }
+});
+Object.defineProperty(exports, "ResourceEditor", {
+  enumerable: true,
+  get: function get() {
+    return _ResourceEditor.default;
+  }
+});
+
+var _Metadata = _interopRequireDefault(require("./components/Metadata"));
+
+var _ProgressBar = _interopRequireDefault(require("./components/ProgressBar"));
+
+var _TablePreview = _interopRequireDefault(require("./components/TablePreview"));
+
+var _TableSchema = _interopRequireDefault(require("./components/TableSchema"));
+
+var _Upload = _interopRequireDefault(require("./components/Upload"));
+
+var _ResourceEditor = _interopRequireDefault(require("./components/ResourceEditor"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
