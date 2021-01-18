5de621d07fc11443298ef4a42d285983817fe256
steveoni <steohenoni2@gmail.com>
2021-01-18 20:47:56 +0100

    [index.js][s]: add main index file to lib/

---
+++ src/lib/index.js
@@ -0,0 +1,15 @@
+import Metadata from  './components/Metadata';
+import ProgressBar from  './components/ProgressBar';
+import TablePreview from  './components/TablePreview';
+import TableSchema from  './components/TableSchema';
+import Upload from './components/Upload';
+import ResourceEditor from './components/ResourceEditor';
+
+export {
+  Metadata,
+  ProgressBar,
+  TablePreview,
+  TableSchema,
+  Upload,
+  ResourceEditor
+};
