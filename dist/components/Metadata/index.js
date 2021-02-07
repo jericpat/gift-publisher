59e57036da5552ed0e65448e06f4b08f30e0671f
steveoni <steohenoni2@gmail.com>
2021-02-07 12:36:06 +0000

    [Metadata][s]: build for Metdata

--- dist/components/Metadata/index.js
+++ dist/components/Metadata/index.js
@@ -14,11 +14,9 @@ var _datapubNocss = require("datapub-nocss");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Metadata = function Metadata(_ref) {
-  var metadata = _ref.metadata,
+  var dataset = _ref.dataset,
       handleChange = _ref.handleChange;
-  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("h3", {
-    className: "metadata-name"
-  }, metadata.path), /*#__PURE__*/_react.default.createElement("div", {
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("div", {
     className: "metadata-form"
   }, /*#__PURE__*/_react.default.createElement("div", {
     className: "metadata-input"
@@ -30,8 +28,10 @@ var Metadata = function Metadata(_ref) {
     type: "text",
     name: "title",
     id: "title",
-    value: metadata.title,
-    onChange: handleChange
+    value: dataset.title,
+    onChange: function onChange(e) {
+      handleChange(e);
+    }
   })), /*#__PURE__*/_react.default.createElement("div", {
     className: "metadata-input"
   }, /*#__PURE__*/_react.default.createElement("label", {
@@ -41,13 +41,14 @@ var Metadata = function Metadata(_ref) {
     className: "metadata-input__input",
     name: "encoding",
     id: "encoding",
-    value: metadata.encoding || "",
-    onChange: handleChange,
+    value: dataset.encoding || "",
+    onChange: function onChange(e) {
+      handleChange(e);
+    },
     required: true
   }, /*#__PURE__*/_react.default.createElement("option", {
-    value: "",
-    disabled: true
-  }, "Select..."), _datapubNocss.encodeData.map(function (item) {
+    value: "utf_8"
+  }, "UTF-8"), _datapubNocss.encodeData.map(function (item) {
     return /*#__PURE__*/_react.default.createElement("option", {
       key: "format-".concat(item.value),
       value: item.value
@@ -60,14 +61,8 @@ var Metadata = function Metadata(_ref) {
   }, "Format"), /*#__PURE__*/_react.default.createElement("select", {
     className: "metadata-input__input",
     name: "format",
-    id: "format",
-    value: (metadata.format || "").toLowerCase(),
-    onChange: handleChange,
-    required: true
+    id: "format"
   }, /*#__PURE__*/_react.default.createElement("option", {
-    value: "",
-    disabled: true
-  }, "Select..."), /*#__PURE__*/_react.default.createElement("option", {
     value: "csv"
   }, "CSV"))), /*#__PURE__*/_react.default.createElement("div", {
     className: "metadata-input"
@@ -79,14 +74,16 @@ var Metadata = function Metadata(_ref) {
     type: "text",
     name: "description",
     id: "description",
-    value: metadata.description || "",
-    onChange: handleChange,
+    value: dataset.description || "",
+    onChange: function onChange(e) {
+      handleChange(e);
+    },
     rows: 4
   }))));
 };
 
 Metadata.propTypes = {
-  metadata: _propTypes.default.object.isRequired,
+  dataset: _propTypes.default.object.isRequired,
   handleChange: _propTypes.default.func.isRequired
 };
 var _default = Metadata;
