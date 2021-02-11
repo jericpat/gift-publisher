e28d2bbd0dcf09de68c7669de2b2b0038f3bca55
Rising Odegua <risingodegua@gmail.com>
2021-02-11 11:23:32 +0100

    [import][s]: Remove local import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./assets/main.css";
 import App from "./App";
 // import * as data from './data.json';
-import { metadataEmpty, metadataSingle, metadataDouble } from "./data/metadata";
+// import { metadataEmpty, metadataSingle, metadataDouble } from "./data/metadata";
 
 // Automatically mount the app if an element with id='ResourceEditor' exists
 const element = document.getElementById("ResourceEditor");
@@ -11,7 +11,7 @@ if (element) {
   const config = {
     authorizedApi: "/api/authorize",
     lfsServerUrl: "https://giftless-gift.herokuapp.com",
-    dataset: metadataSingle, //for test purpose, suppose to be provided by portal
+    dataset: {}, //for test purpose, suppose to be provided by portal
     metastoreApi: "/api/dataset",
   };
 
