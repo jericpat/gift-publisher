b124d50992becd09a44aef9b9c2072af5765a3c1
Rising Odegua <risingodegua@gmail.com>
2021-02-15 17:25:05 +0100

    [utils][s]: Remove var declaration

--- src/utils/index.js
+++ src/utils/index.js
@@ -15,7 +15,7 @@ const removeHyphen = (id) => {
 };
 
 const isValidURL = (val) => {
-  var res = val.match(
+  const res = val.match(
     /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
   );
   return res !== null;
