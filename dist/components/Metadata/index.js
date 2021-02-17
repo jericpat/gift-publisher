a4eb8812c0fdea59e238081cd7e7aacaf0f57642
Rising Odegua <risingodegua@gmail.com>
2021-02-17 15:41:47 +0100

    Build new version of publsiher

--- dist/components/Metadata/index.js
+++ dist/components/Metadata/index.js
@@ -5,25 +5,54 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = require("react");
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
 
 var _datapubNocss = require("datapub-nocss");
+
+var _countries = _interopRequireDefault(require("../../db/countries.json"));
+
+var _react2 = _interopRequireDefault(require("@yaireo/tagify/dist/react.tagify"));
+
+require("@yaireo/tagify/dist/tagify.css");
+
+require("./Metadata.css");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Metadata = function Metadata(_ref) {
   var dataset = _ref.dataset,
       handleChange = _ref.handleChange;
-  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("div", {
-    className: "metadata-form"
-  }, /*#__PURE__*/_react.default.createElement("div", {
+  var tagifySettings = {
+    whitelist: ["Finance", "Tabular", "Economics", "Trade", "Fiscal"],
+    maxTags: 6,
+    backspace: "edit",
+    addTagOnBlur: false,
+    dropdown: {
+      enabled: 0
+    }
+  };
+
+  var handleTagChange = function handleTagChange(values) {
+    values = JSON.parse(values);
+    values = values.map(function (value) {
+      return Object.values(value)[0];
+    });
+    var customEvent = {
+      target: {
+        value: values,
+        name: "tags"
+      }
+    };
+    handleChange(customEvent);
+  };
+
+  return /*#__PURE__*/_react.React.createElement(_react.React.Fragment, null, /*#__PURE__*/_react.React.createElement("h2", null, "Mandatory fields are marked with an asterisks(", /*#__PURE__*/_react.React.createElement("span", {
+    className: "ast-important"
+  }, "*"), ")"), /*#__PURE__*/_react.React.createElement("div", {
     className: "metadata-input"
-  }, /*#__PURE__*/_react.default.createElement("label", {
-    className: "metadata-label",
-    htmlFor: "title"
-  }, "Title"), /*#__PURE__*/_react.default.createElement("input", {
+  }, /*#__PURE__*/_react.React.createElement("input", {
     className: "metadata-input__input",
     type: "text",
     name: "title",
@@ -32,44 +61,14 @@ var Metadata = function Metadata(_ref) {
     onChange: function onChange(e) {
       handleChange(e);
     }
-  })), /*#__PURE__*/_react.default.createElement("div", {
-    className: "metadata-input"
-  }, /*#__PURE__*/_react.default.createElement("label", {
+  }), /*#__PURE__*/_react.React.createElement("label", {
     className: "metadata-label",
-    htmlFor: "encoding"
-  }, "Encoding"), /*#__PURE__*/_react.default.createElement("select", {
-    className: "metadata-input__input",
-    name: "encoding",
-    id: "encoding",
-    value: dataset.encoding || "",
-    onChange: function onChange(e) {
-      handleChange(e);
-    },
-    required: true
-  }, /*#__PURE__*/_react.default.createElement("option", {
-    value: "utf_8"
-  }, "UTF-8"), _datapubNocss.encodeData.map(function (item) {
-    return /*#__PURE__*/_react.default.createElement("option", {
-      key: "format-".concat(item.value),
-      value: item.value
-    }, item.label);
-  }))), /*#__PURE__*/_react.default.createElement("div", {
+    htmlFor: "title"
+  }, /*#__PURE__*/_react.React.createElement("span", {
+    className: "ast-important"
+  }, "*"), "Title of the dataset")), /*#__PURE__*/_react.React.createElement("div", {
     className: "metadata-input"
-  }, /*#__PURE__*/_react.default.createElement("label", {
-    className: "metadata-label",
-    htmlFor: "format"
-  }, "Format"), /*#__PURE__*/_react.default.createElement("select", {
-    className: "metadata-input__input",
-    name: "format",
-    id: "format"
-  }, /*#__PURE__*/_react.default.createElement("option", {
-    value: "csv"
-  }, "CSV"))), /*#__PURE__*/_react.default.createElement("div", {
-    className: "metadata-input"
-  }, /*#__PURE__*/_react.default.createElement("label", {
-    className: "metadata-label",
-    htmlFor: "description"
-  }, "Description"), /*#__PURE__*/_react.default.createElement("textarea", {
+  }, /*#__PURE__*/_react.React.createElement("textarea", {
     className: "metadata-input__textarea",
     type: "text",
     name: "description",
@@ -78,8 +77,200 @@ var Metadata = function Metadata(_ref) {
     onChange: function onChange(e) {
       handleChange(e);
     },
-    rows: 4
-  }))));
+    rows: 5
+  }), /*#__PURE__*/_react.React.createElement("label", {
+    className: "metadata-label",
+    htmlFor: "description"
+  }, /*#__PURE__*/_react.React.createElement("span", {
+    className: "ast-important"
+  }, "*"), "Description of the dataset")), /*#__PURE__*/_react.React.createElement("div", {
+    className: "metadata-form"
+  }, /*#__PURE__*/_react.React.createElement("div", null, /*#__PURE__*/_react.React.createElement("div", {
+    className: "metadata-input"
+  }, /*#__PURE__*/_react.React.createElement("select", {
+    className: "metadata-input__input",
+    name: "continent",
+    id: "continent",
+    value: dataset.continent || "",
+    onChange: function onChange(e) {
+      handleChange(e);
+    },
+    required: true
+  }, /*#__PURE__*/_react.React.createElement("option", {
+    value: ""
+  }, "Select"), /*#__PURE__*/_react.React.createElement("option", {
+    value: "Europe"
+  }, "Europe"), /*#__PURE__*/_react.React.createElement("option", {
+    value: "Africa"
+  }, "Africa"), /*#__PURE__*/_react.React.createElement("option", {
+    value: "Asia"
+  }, "Asia"), /*#__PURE__*/_react.React.createElement("option", {
+    value: "North America"
+  }, "North America"), /*#__PURE__*/_react.React.createElement("option", {
+    value: "South America"
+  }, "South America"), /*#__PURE__*/_react.React.createElement("option", {
+    value: "Australia"
+  }, "Australia")), /*#__PURE__*/_react.React.createElement("label", {
+    className: "metadata-label",
+    htmlFor: "continent"
+  }, "Continent")), /*#__PURE__*/_react.React.createElement("div", {
+    className: "metadata-input"
+  }, /*#__PURE__*/_react.React.createElement("select", {
+    className: "metadata-input__input",
+    name: "country",
+    id: "country",
+    value: dataset.country || "",
+    onChange: function onChange(e) {
+      handleChange(e);
+    },
+    required: true
+  }, /*#__PURE__*/_react.React.createElement("option", {
+    value: ""
+  }, "Select"), _countries.default.map(function (item) {
+    return /*#__PURE__*/_react.React.createElement("option", {
+      key: "format-".concat(item.text),
+      value: item.text
+    }, item.text);
+  })), /*#__PURE__*/_react.React.createElement("label", {
+    className: "metadata-label",
+    htmlFor: "encoding"
+  }, "Country"))), /*#__PURE__*/_react.React.createElement("div", {
+    className: "metadata-input"
+  }, /*#__PURE__*/_react.React.createElement(_react2.default, {
+    mode: "textarea",
+    className: "tags",
+    name: "tags",
+    settings: tagifySettings,
+    value: "tags" in dataset ? dataset.tags.toString() : "",
+    onChange: function onChange(e) {
+      return e.persist(), handleTagChange(e.target.value);
+    }
+  }), /*#__PURE__*/_react.React.createElement("label", {
+    className: "metadata-label",
+    htmlFor: "tags"
+  }, "Tags"))), /*#__PURE__*/_react.React.createElement("div", {
+    className: "metadata-form"
+  }, /*#__PURE__*/_react.React.createElement("div", {
+    className: "metadata-input"
+  }, /*#__PURE__*/_react.React.createElement("input", {
+    className: "metadata-input__input",
+    type: "text",
+    name: "region",
+    id: "region",
+    value: dataset.region,
+    onChange: function onChange(e) {
+      handleChange(e);
+    }
+  }), /*#__PURE__*/_react.React.createElement("label", {
+    className: "metadata-label",
+    htmlFor: "region"
+  }, "Region")), /*#__PURE__*/_react.React.createElement("div", {
+    className: "metadata-input"
+  }, /*#__PURE__*/_react.React.createElement("input", {
+    className: "metadata-input__input",
+    type: "text",
+    name: "author_website",
+    id: "author_website",
+    value: dataset.author_website,
+    onChange: function onChange(e) {
+      handleChange(e);
+    }
+  }), /*#__PURE__*/_react.React.createElement("label", {
+    className: "metadata-label",
+    htmlFor: "author_website"
+  }, "Author's Website"))), /*#__PURE__*/_react.React.createElement("div", {
+    className: "metadata-form"
+  }, /*#__PURE__*/_react.React.createElement("div", {
+    className: "metadata-input"
+  }, /*#__PURE__*/_react.React.createElement("input", {
+    className: "metadata-input__input",
+    type: "text",
+    name: "city",
+    id: "city",
+    value: dataset.city,
+    onChange: function onChange(e) {
+      handleChange(e);
+    }
+  }), /*#__PURE__*/_react.React.createElement("label", {
+    className: "metadata-label",
+    htmlFor: "city"
+  }, "City")), /*#__PURE__*/_react.React.createElement("div", {
+    className: "metadata-input"
+  }, /*#__PURE__*/_react.React.createElement("input", {
+    className: "metadata-input__input",
+    type: "text",
+    name: "author_email",
+    id: "author_email",
+    value: dataset.author_email,
+    onChange: function onChange(e) {
+      handleChange(e);
+    }
+  }), /*#__PURE__*/_react.React.createElement("label", {
+    className: "metadata-label",
+    htmlFor: "author_email"
+  }, "Author's Email Address"))), /*#__PURE__*/_react.React.createElement("br", null), /*#__PURE__*/_react.React.createElement("div", {
+    className: "metadata-form"
+  }, /*#__PURE__*/_react.React.createElement("div", null, /*#__PURE__*/_react.React.createElement("h1", null, /*#__PURE__*/_react.React.createElement("b", null, "Fiscal Period")), /*#__PURE__*/_react.React.createElement("div", {
+    className: "metadata-input"
+  }, /*#__PURE__*/_react.React.createElement("input", {
+    className: "metadata-input__input",
+    type: "date",
+    name: "start_date",
+    id: "start_date",
+    value: dataset.start_date,
+    onChange: function onChange(e) {
+      handleChange(e);
+    }
+  }), /*#__PURE__*/_react.React.createElement("label", {
+    className: "metadata-label",
+    htmlFor: "start_date"
+  }, /*#__PURE__*/_react.React.createElement("span", {
+    className: "ast-important"
+  }, "*"), "Starting date (month-day-year)")), /*#__PURE__*/_react.React.createElement("div", {
+    className: "metadata-input"
+  }, /*#__PURE__*/_react.React.createElement("input", {
+    className: "metadata-input__input",
+    type: "date",
+    name: "end_date",
+    id: "end_date",
+    value: dataset.end_date,
+    onChange: function onChange(e) {
+      handleChange(e);
+    }
+  }), /*#__PURE__*/_react.React.createElement("label", {
+    className: "metadata-label",
+    htmlFor: "end_date"
+  }, /*#__PURE__*/_react.React.createElement("span", {
+    className: "ast-important"
+  }, "*"), "Ending date (month-day-year)"))), /*#__PURE__*/_react.React.createElement("div", null, /*#__PURE__*/_react.React.createElement("h1", null, /*#__PURE__*/_react.React.createElement("b", null, "Encoding and file format")), /*#__PURE__*/_react.React.createElement("div", null, /*#__PURE__*/_react.React.createElement("select", {
+    className: "metadata-input__input",
+    name: "encoding",
+    id: "encoding",
+    value: dataset.encoding || "",
+    onChange: function onChange(e) {
+      handleChange(e);
+    },
+    required: true
+  }, /*#__PURE__*/_react.React.createElement("option", {
+    value: "utf_8"
+  }, "UTF-8"), _datapubNocss.encodeData.map(function (item) {
+    return /*#__PURE__*/_react.React.createElement("option", {
+      key: "format-".concat(item.value),
+      value: item.value
+    }, item.label);
+  })), /*#__PURE__*/_react.React.createElement("label", {
+    className: "metadata-label",
+    htmlFor: "encoding"
+  }, "File encoding: If you're unsure about this setting, please use UTF-8")), /*#__PURE__*/_react.React.createElement("div", null, /*#__PURE__*/_react.React.createElement("select", {
+    className: "metadata-input__input",
+    name: "format",
+    id: "format"
+  }, /*#__PURE__*/_react.React.createElement("option", {
+    value: "csv"
+  }, "CSV"))), /*#__PURE__*/_react.React.createElement("label", {
+    className: "metadata-label",
+    htmlFor: "format"
+  }, "File format"))));
 };
 
 Metadata.propTypes = {
--- src/lib/components/DatasetEditor/index.js
+++ src/lib/components/DatasetEditor/index.js
@@ -13,6 +13,17 @@ export class DatasetEditor extends React.Component {
   constructor(props) {
     super(props);
     const dataset = props.config.dataset;
+    dataset.encoding = "utf_8";
+    dataset.format = "csv";
+    if (
+      !("sample" in dataset) &&
+      "resources" in dataset &&
+      dataset["resources"].length > 0
+    ) {
+      dataset["sample"] = dataset["resources"][0]["sample"];
+    } else {
+      dataset["sample"] = [];
+    }
     this.state = {
       dataset,
       resource: {}, //This will hold the uploaded resource metadata
@@ -27,12 +38,13 @@ export class DatasetEditor extends React.Component {
       },
       client: null,
       isResourceEdit: false,
-      currentStep: 0,
+      currentStep: props.config.skipUpload ? 4 : 0,
       richTypeFilled: false,
       saveButtonText: "Save",
     };
     this.metadataHandler = this.metadataHandler.bind(this);
     this.handleRichTypeCount = this.handleRichTypeCount.bind(this);
+
   }
 
   metadataHandler(resource) {
@@ -58,8 +70,9 @@ export class DatasetEditor extends React.Component {
     resource["mediatype"] = fileResource.type;
     resource["name"] = fileResource.name;
     resource["dialect"] = fileResource.dialect;
-    resource["path"] = `data/${fileResource.name}`;
-    resource["title"] = fileResource["name"].split(".")[0];
+    resource["path"] = fileResource.path;
+    //   "path" in fileResource ? fileResource.path : `data/${fileResource.name}`;
+    // resource["title"] = fileResource["name"].split(".")[0];
 
     if (Object.keys(dataset).includes("resources")) {
       dataset.resources.push(resource);
@@ -72,6 +85,10 @@ export class DatasetEditor extends React.Component {
     resource["sample"] = fileResource.sample;
     resource["columns"] = fileResource.columns;
 
+    if (dataset["sample"].length == 0) {
+      dataset["sample"] = fileResource.sample;
+    }
+
     return { dataset, resource };
   }
 
@@ -83,8 +100,7 @@ export class DatasetEditor extends React.Component {
         richTypeFilled: true,
       });
     }
-  }
-
+  };
 
   handleChangeMetadata = (event) => {
     const target = event.target;
@@ -130,30 +146,26 @@ export class DatasetEditor extends React.Component {
 
   deleteResource = (hash) => {
     const { dataset } = { ...this.state };
-    if (window.confirm("Are you sure to delete this resource?")) {
-      if (dataset.resources.length == 1) {
-        dataset.resources = [];
-        this.setState({ dataset, resource: {} });
+    if (window.confirm("Are you sure you want to delete this resource?")) {
+      const temp_dataset = { ...dataset };
+      if (temp_dataset.resources.length == 1) {
+        temp_dataset.resources = [];
       } else {
-        const newResource = dataset.resources.filter(
+        const newResource = temp_dataset.resources.filter(
           (resource) => resource.hash != hash
         );
-        dataset.resources = newResource;
-        this.setState({
-          dataset,
-          resource: {},
-        });
+        temp_dataset.resources = newResource;
       }
-
       axios({
         method: "post",
-        url: `/api/dataset/${this.state.dataset.name}`,
+        url: `/api/dataset/${temp_dataset.name}`,
         data: {
-          metadata: this.state.dataset,
-          description: this.state.dataset.description,
+          metadata: temp_dataset,
+          description: temp_dataset.description,
         },
       }).then(
         (response) => {
+          this.setState({ temp_dataset, resource: {} });
           alert("Resource has been removed sucessfully");
         },
         (error) => {
@@ -182,15 +194,19 @@ export class DatasetEditor extends React.Component {
     this.setState({ ui: newUiState });
     if (status.success && !status.loading) {
       this.nextScreen();
-    }
-    if (!status.success && status.error) {
+    } else if (!status.success && status.error) {
+      const dataset = { ...this.state.dataset };
+      if ("resources" in dataset && dataset["resources"].length > 0) {
+        dataset.resources.pop();
+      }
+      this.setState({ dataset });
       this.prevScreen();
     }
 
     //clears error message after 6 seconds
     setTimeout(() => {
       this.setState({ ui: { ...this.state.ui, errorMsg: "" } });
-    }, 6000);
+    }, 10000);
   };
 
   onChangeResourceId = (resourceId) => {
@@ -266,7 +282,7 @@ export class DatasetEditor extends React.Component {
                   Provide your data file
                 </h1>
                 <h2 className="upload-header__title_h2">
-                  Supported formats: csv, xlsx, xls
+                  Supported format: CSV
                 </h2>
               </div>
 
@@ -317,7 +333,7 @@ export class DatasetEditor extends React.Component {
             {this.state.currentStep == 4 && !this.state.savedDataset && (
               <>
                 <div className="upload-header">
-                  <h1 className="upload-header__title_h1">Provide Metadata</h1>
+                  <h1 className="upload-header__title_h1">Describe Metadata</h1>
                 </div>
                 <Metadata
                   dataset={this.state.dataset}
@@ -327,18 +343,22 @@ export class DatasetEditor extends React.Component {
             )}
           </div>
         </form>
+
         <div className="resource-edit-actions">
           {this.state.currentStep == 4 &&
             !this.state.isResourceEdit &&
             this.state.resource && (
-              <button className="btn" onClick={this.handleSaveDataset}>
+              <button className="btn-save" onClick={this.handleSaveDataset}>
                 {this.state.saveButtonText}
               </button>
             )}
           {this.state.currentStep == 4 &&
             !this.state.isResourceEdit &&
             this.state.resource && (
-              <button className="btn" onClick={this.downloadDatapackage}>
+              <button
+                className="btn-download"
+                onClick={this.downloadDatapackage}
+              >
                 Download Package
               </button>
             )}
