aecbe8086f0ab2001766d34b2a52ffb1dd2522c3
Rising Odegua <risingodegua@gmail.com>
2021-06-02 10:12:27 +0100

    [Tableschema][m]: Fix existing type display

--- src/components/TableSchema/index.jsx
+++ src/components/TableSchema/index.jsx
@@ -142,26 +142,49 @@ const TableSchema = (props) => {
 
   const renderEditSchemaField = (key) => {
     if (key === "type") {
-      return schema.fields.map((item, index) => (
-        <td key={`schema-type-field-${key}-${index}`}>
-          <select
-            className="table-tbody-select"
-            value={item[key] || ""}
-            onChange={(event) => handleChange(event, key, index)}
-          >
-            {types.type.map((item, index) => {
-              return (
-                <option
-                  key={`schema-type-field-option-${item}-${index}`}
-                  value={item}
-                >
-                  {item}
-                </option>
-              );
-            })}
-          </select>
-        </td>
-      ));
+      if (
+        props.dataset.resources &&
+        props.dataset.resources.length > 1 &&
+        resourceHasRichType(props.dataset)
+      ) {
+        const existingTypes = props.dataset.resources[0].schema.fields.map(
+          (field) => {
+            return field.type;
+          }
+        );
+        //The schema already exists. Prefill and set to uneditable
+        return existingTypes.map((item, index) => (
+          <td key={`schema-type-field-${key}-${index}`}>
+            <input
+              className="table-tbody-input"
+              type="text"
+              value={item}
+              disabled
+            />
+          </td>
+        ));
+      } else {
+        return schema.fields.map((item, index) => (
+          <td key={`schema-type-field-${key}-${index}`}>
+            <select
+              className="table-tbody-select"
+              value={item[key] || ""}
+              onChange={(event) => handleChange(event, key, index)}
+            >
+              {types.type.map((item, index) => {
+                return (
+                  <option
+                    key={`schema-type-field-option-${item}-${index}`}
+                    value={item}
+                  >
+                    {item}
+                  </option>
+                );
+              })}
+            </select>
+          </td>
+        ));
+      }
     }
     //style for column rich type select field
     const customStyles = {
