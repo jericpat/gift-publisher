56a0800e00e2da3ad69e5c77f16a914b431f8e65
steveoni <steohenoni2@gmail.com>
2021-08-17 17:52:10 +0000

    [inputfile][s]: merge update from main to package

--- src/lib/components/InputFile/index.js
+++ src/lib/components/InputFile/index.js
@@ -23,12 +23,12 @@ const InputFile = ({ onChangeHandler }) => {
                     or <br />
                     click to select
                 </span>
-            </div>
-      )
-  }
+        </div>
+    )
+}
 
 InputFile.propTypes = {
-  onChangeHandler: PropTypes.func.isRequired
+    onChangeHandler: PropTypes.func.isRequired
 };
 
 export default InputFile;
